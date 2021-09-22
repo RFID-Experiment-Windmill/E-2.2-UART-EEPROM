6c52eaab49d7bcdf5ac9351d68a3f6e6e290e9c7
Windmill-City <1449182174@qq.com>
2021-09-22 21:31:59 +0800

    Fix Compile time SysClock

--- include/STC/STCBase.h
+++ include/STC/STCBase.h
@@ -14,9 +14,26 @@
 #define FreqOsc (11059200)
 
 /**
+ * @brief Possible value of the CLK_DIV
+ */
+#define ClockDivision_0 0x0
+#define ClockDivision_2 0x1
+#define ClockDivision_4 0x2
+#define ClockDivision_8 0x3
+#define ClockDivision_16 0x4
+#define ClockDivision_32 0x5
+#define ClockDivision_64 0x6
+#define ClockDivision_128 0x7
+
+/**
+ * @brief Division of the oscillator
+ */
+#define ClockDivision ClockDivision_0
+
+/**
  * @brief Frequency of the System Clock
  */
-#define SysClock (FreqOsc << (CLK_DIV & 0x7))
+#define SysClock (FreqOsc << (ClockDivision))
 
 /**
  * @brief How many SysClock passes in 1 millisecond
